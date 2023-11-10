880b3b11d87379cb3ed376700d5015fc0c3c2d69
andriy-kolokolov <koloandre22@gmail.com>
2023-11-10 15:59:45 +0100

    rework seeder

--- src/main/java/com/example/experis/database/PizzaSeeder.java
+++ src/main/java/com/example/experis/database/PizzaSeeder.java
@@ -20,29 +20,31 @@ public class PizzaSeeder {
     private static final String[] INGREDIENTS = {
             "Mozzarella", "Tomato Sauce", "Pepperoni", "Onions",
             "Green Peppers", "Mushrooms", "Sausage", "Bacon",
-            "Black Olives", "Pineapple", "Ham", "Spinach"
+            "Black Olives", "Pineapple", "Ham", "Spinach", "Cheddar Cheese",
+            "Feta Cheese", "Gouda Cheese", "Roquefort Cheese", "Camembert Cheese",
     };
 
+    // without duplicates list
     private static final String[] PIZZA_NAMES = {
             "Margherita", "Pepperoni", "Hawaiian", "Veggie",
             "Meat Lovers", "Supreme", "BBQ Chicken", "Buffalo Chicken",
-            "Mushroom and Spinach", "Four Cheese"
+            "Mushroom and Spinach", "Four Cheese",
+            "Chicken Delight", "Chicken Tikka Masala",
     };
 
     private static final Random random = new Random();
-    private static final int MAX_PIZZA_COUNT = 15;
 
     @Bean
     CommandLineRunner commandLineRunner(PizzaRepository pizzaRepository) {
         return args -> {
             long count = pizzaRepository.count();
-            if (count >= MAX_PIZZA_COUNT) {
+            if (count >= PIZZA_NAMES.length) {
                 return; // Skip data generation if the threshold is reached
             }
 
             Faker faker = new Faker(new Locale("en-US"));
 
-            for (int i = 0; i < MAX_PIZZA_COUNT; i++) {
+            for (int i = 0; i < PIZZA_NAMES.length; i++) {
                 String name = PIZZA_NAMES[random.nextInt(PIZZA_NAMES.length)];
 
                 String description = IntStream.range(0, 3 + random.nextInt(3)) // 3 to 5 ingredients
